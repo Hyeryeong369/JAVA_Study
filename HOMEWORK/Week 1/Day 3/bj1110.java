9f6b9612e4431b58336918c9b951d17db4ee3667
Hyeryeong369 <hyeryeong369@naver.com>
2022-01-11 22:02:13 +0900

    Day3 JAVA Study

---
+++ HOMEWORK/Week 1/Day 3/bj1110.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class bj1110 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        
+    }
+}
