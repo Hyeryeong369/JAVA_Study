5da52fa19662fb432410ec930dcb2722dda2551d
Hyeryeong369 <hyeryeong369@naver.com>
2022-01-13 15:53:52 +0900

    Day3 JAVA Study

--- HOMEWORK/Week 1/Day 3/bj1110.java
+++ HOMEWORK/Week 1/Day 3/bj1110.java
@@ -4,7 +4,20 @@ public class bj1110 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        sc.close();
 
-        
+        int copy = N;
+        int cnt = 0;
+
+        while (true) {
+            N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+                // 십의 자리         일의 자리
+            cnt++;
+
+            if(copy == N) {
+                break;
+            }
+        }
+        System.out.println(cnt);        
     }
 }
