390837a9772ff6d78acae3538b6441620986f6f5
Hyeryeong369 <hyeryeong369@naver.com>
2022-01-14 11:27:30 +0900

    Day4 JAVA Study

--- HOMEWORK/Week 1/Day 4/bj10809.java
+++ HOMEWORK/Week 1/Day 4/bj10809.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class bj10809 {
     public static void main(String[] args) {
         Scanner sc =  new Scanner(System.in);
-        String number = sc.next();
+        String word = sc.next();
         sc.close();
 
         for (char N = 'a'; N <= 'z'; N++) {
-            System.out.print(number.indexOf(N) + " ");      // indexOf("찾을 특정 문자", "시작할 위치")
+            System.out.print(word.indexOf(N) + " ");      // indexOf("찾을 특정 문자", "시작할 위치")
         }
     }
 }
+
